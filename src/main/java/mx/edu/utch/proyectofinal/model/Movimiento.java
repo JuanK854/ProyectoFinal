0ba9c44078daa79f61f8d9414ee5bd7eb9206c18
JuanK854 <juanc854ypk@gmail.com>
2025-08-07 19:01:19 -0600

    Se crea la clase Movimiento.java

--- src/main/java/mx/edu/utch/proyectofinal/model/Movimiento.java
+++ src/main/java/mx/edu/utch/proyectofinal/model/Movimiento.java
@@ -1,69 +1,61 @@
 package mx.edu.utch.proyectofinal.model;
 
-import javafx.beans.property.*;
-import java.time.LocalDate;
-
 public class Movimiento {
+    private String fecha;       // ej. "2025-08-08"
+    private String tipo;        // "Ingreso" o "Gasto"
+    private double monto;       // número positivo
+    private String descripcion; // texto corto
 
-    private final ObjectProperty<LocalDate> fecha;
-    private final StringProperty tipo;
-    private final DoubleProperty monto;
-    private final StringProperty descripcion;
-
-    public Movimiento(LocalDate fecha, String tipo, double monto, String descripcion) {
-        this.fecha = new SimpleObjectProperty<>(fecha);
-        this.tipo = new SimpleStringProperty(tipo);
-        this.monto = new SimpleDoubleProperty(monto);
-        this.descripcion = new SimpleStringProperty(descripcion);
+    public  Movimiento(){
+        this(" "," ",0," ");
     }
 
-    // Getters y Setters con propiedades para JavaFX
-    public LocalDate getFecha() {
-        return fecha.get();
+    public Movimiento(String fecha, String tipo, double monto, String descripcion) {
+        this.fecha = fecha;
+        this.tipo = tipo;
+        this.monto = monto;
+        this.descripcion = descripcion;
     }
 
-    public void setFecha(LocalDate fecha) {
-        this.fecha.set(fecha);
-    }
-
-    public ObjectProperty<LocalDate> fechaProperty() {
+    public String getFecha() {
         return fecha;
     }
 
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
     public String getTipo() {
-        return tipo.get();
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo.set(tipo);
-    }
-
-    public StringProperty tipoProperty() {
         return tipo;
     }
 
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
     public double getMonto() {
-        return monto.get();
-    }
-
-    public void setMonto(double monto) {
-        this.monto.set(monto);
-    }
-
-    public DoubleProperty montoProperty() {
         return monto;
     }
 
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
     public String getDescripcion() {
-        return descripcion.get();
+        return descripcion;
     }
 
     public void setDescripcion(String descripcion) {
-        this.descripcion.set(descripcion);
+        this.descripcion = descripcion;
     }
 
-    public StringProperty descripcionProperty() {
-        return descripcion;
+    @Override
+    public String toString() {
+        return "Movimiento{" +
+                "fecha='" + fecha + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", monto=" + monto +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
     }
 }
-
